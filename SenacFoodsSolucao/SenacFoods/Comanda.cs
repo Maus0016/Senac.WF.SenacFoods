54ea080470cf7c270280cdfb0bd20076c6448862
Daniel Maus <hakumennomodo@gmail.com>
2025-06-12 22:00:51 -0300

    CRIACAO DO EMMIGRAFITS

--- SenacFoodsSolucao/SenacFoods/Comanda.cs
+++ SenacFoodsSolucao/SenacFoods/Comanda.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,9 @@ namespace SenacFoods
 {
     public class Comanda
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
         public int Id { get; set; }
         public int NumeroMesa { get; set; }
         public int NomeCliente {  get; set; }
--- SenacFoodsSolucao/SenacFoods/ComandaDBContext.cs
+++ SenacFoodsSolucao/SenacFoods/ComandaDBContext.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace SenacFoods
+namespace SenacFoods;
+
+public class ComandaDBContext : DbContext
 {
-    public class ComandaDBContext : DbContext
+    //1 Construtor do banco de dados
+    public ComandaDBContext() : base()
     {
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<CardapioItem> CardapioItems { get; set; }
-        public DbSet<Mesa> Mesas { get; set; }
-        public DbSet<Comanda> Comandas { get; set; }
+        
     }
+    //2 Configurar conexão
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //2.1 String de conexão
+        var conexao = "Server=localhost;Database=SenacFoods;User=root;Password=";
+        //2.2 Configuras o provedor de banco de dados
+        optionsBuilder.UseMySql(conexao, ServerVersion.AutoDetect(conexao));
+        base.OnConfiguring(optionsBuilder);
+    }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<CardapioItem> CardapioItems { get; set; }
+    public DbSet<Mesa> Mesas { get; set; }
+    public DbSet<Comanda> Comandas { get; set; }
+    public DbSet<ComandaItem> ComandasItem { get; set; }
+    public DbSet<PedidoCozinha> PedidoCozinhas { get; set; }
+    public DbSet<PedidoCozinhaItem> PedidoCozinhaItems { get; set; }
 }
